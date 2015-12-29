4f92a50af18136a2d72012c3ef6ad602b002d84c
Brian Nenninger <bnenning@gmail.com>
2015-12-28 22:29:02 -0500

    Fix upside down images on Nexus 5x and 6.

--- src/com/dozingcatsoftware/util/ARManager.java
+++ src/com/dozingcatsoftware/util/ARManager.java
@@ -6,55 +6,55 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class ARManager implements SurfaceHolder.Callback {
-	
+
 	Activity activity;
 	SurfaceView cameraView;
 	Camera.PreviewCallback previewCallback;
 	Runnable cameraOpenedCallback;
 	Runnable cameraStartedCallback;
 	boolean cameraInitialized = false;
-	
+
 	Camera camera;
 	boolean cameraViewReady = false;
     int cameraId = 0;
 
 	int preferredPreviewWidth = 0, preferredPreviewHeight = 0;
 	int numPreviewCallbackBuffers = 0;
-	
+
 	public ARManager(Activity _activity, SurfaceView _cameraView, Camera.PreviewCallback _previewCallback) {
 		this.activity = _activity;
 		this.cameraView = _cameraView;
 		this.previewCallback = _previewCallback;
 	}
-	
+
 	public void setupCameraView() {
     	cameraView.getHolder().addCallback(this);
     	cameraView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
-	
+
 	public static ARManager createAndSetupCameraView(Activity _activity, SurfaceView _cameraView, Camera.PreviewCallback _previewCallback) {
 		ARManager manager = new ARManager(_activity, _cameraView, _previewCallback);
 		manager.setupCameraView();
 		return manager;
 	}
-	
+
 	public void setPreferredPreviewSize(int width, int height) {
 		this.preferredPreviewWidth = width;
 		this.preferredPreviewHeight = height;
 	}
-	
+
 	public void setNumberOfPreviewCallbackBuffers(int n) {
 		this.numPreviewCallbackBuffers = n;
 	}
-	
+
 	public void setCameraOpenedCallback(Runnable callback) {
 		cameraOpenedCallback = callback;
 	}
-	
+
 	public void setCameraStartedCallback(Runnable callback) {
 		cameraStartedCallback = callback;
 	}
-	
+
 	public boolean startCamera() {
 		if (camera==null) {
 			try {
@@ -74,6 +74,16 @@ public class ARManager implements SurfaceHolder.Callback {
 	            else {
 		            camera.setPreviewCallback(this.previewCallback);
 	            }
+
+	            if (CameraUtils.getCameraInfo(this.cameraId).isRotated180Degrees()) {
+	                // We need both camera.setDisplayOrientation to rotate the preview image, and
+	                // Camera.Parameters.setRotation to rotate the actual pictures that we take.
+	               camera.setDisplayOrientation(180);
+	               Camera.Parameters params = camera.getParameters();
+	               params.setRotation(180);
+	               camera.setParameters(params);
+	            }
+
 	            camera.startPreview();
 	            if (!cameraInitialized && cameraStartedCallback!=null) {
 	            	cameraStartedCallback.run();
@@ -87,13 +97,13 @@ public class ARManager implements SurfaceHolder.Callback {
 		return (camera!=null);
 	}
 
-	
+
 	public void startCameraIfVisible() {
 		if (cameraViewReady) {
 			startCamera();
 		}
 	}
-	
+
 	public void stopCamera() {
 		if (camera!=null) {
 			camera.setPreviewCallback(null);
@@ -102,7 +112,7 @@ public class ARManager implements SurfaceHolder.Callback {
 	    	camera = null;
 		}
 	}
-	
+
     public void switchToCamera(int _cameraId) {
     	if (camera!=null) {
     		stopCamera();
@@ -115,9 +125,9 @@ public class ARManager implements SurfaceHolder.Callback {
     public void switchToNextCamera() {
     	switchToCamera((cameraId + 1) % CameraUtils.numberOfCameras());
     }
-	
+
     // SurfaceHolder callbacks
-	@Override 
+	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		this.cameraViewReady = true;
 		startCameraIfVisible();
@@ -134,7 +144,7 @@ public class ARManager implements SurfaceHolder.Callback {
 		stopCamera();
 	}
 
-	
+
 	public Camera getCamera() {
 		return camera;
 	}
